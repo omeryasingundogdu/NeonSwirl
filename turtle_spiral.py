98cdd31cdb334b8e4919629ec2403a4eb8f355c8
Ömer Yasin gündoğdu <omeryasingundoggdu@gmail.com>
2025-02-11 15:06:24 +0300

    Update turtle_spiral.py

--- turtle_spiral.py
+++ turtle_spiral.py
@@ -1,17 +1,14 @@
 from turtle import *
-import time  # time modülü eklendi
-  # Program başlamadan önce 3 saniye bekler
 
-speed(0)  # En yüksek hız (250 yerine 0 kullan)
+speed(0)  # For the highest speed, use `0` instead of `250`.
 color('cyan')
 bgcolor('black')
 
 b = 1
-time.sleep(3)  
 
 while b <= 350:
     left(b)
     forward(b * 1)
     b += 1  
 
-done()  # Çizim tamamlandıktan sonra pencereyi açık tutar
+done() 
